bad561211a6da043121a27306a4e60a85c514043
Darin_Min <76813856+R-203@users.noreply.github.com>
2021-12-12 21:44:09 +0700

    Update play.py

--- play.py
+++ play.py
@@ -3,4 +3,4 @@ import os,sys,time
 if __name__ == '__main__':
   print('\033[0;33mTerima Kasih Telah menggunakan tool ini...')
   time.sleep(3)
-  os.system('python all.py')
+  os.system('python otp.pyc')
