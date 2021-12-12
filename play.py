bae38bd32e3f0fc148c8aded7189e64d5f5c356c
Darin_Min <76813856+R-203@users.noreply.github.com>
2021-12-12 21:30:59 +0700

    Update play.py

--- play.py
+++ play.py
@@ -1,6 +1,6 @@
 import os,sys,time
 
 if __name__ == '__main__':
-  print('Terima Kasih Telah menggunakan tool ini...')
+  print('\033[0;33mTerima Kasih Telah menggunakan tool ini...')
   time.sleep(3)
   os.system('python all.py')
